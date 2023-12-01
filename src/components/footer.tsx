732ab4330417c18b4d407d14fb7c85f2a958df87
Emelie Baker <emeliefnbaker@gmail.com>
2023-12-01 13:30:04 +0100

    Gjort footern responsiv

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,81 +1,62 @@
 import Image from "next/image";
+import Socials from "./socials";
 
 interface FooterProps {}
 
 const Footer: React.FC<FooterProps> = () => {
   return (
-    <footer className="bg-button-green text-white h-[516px] flex items-center absolute w-full">
-      <div className="flex justify-between px-56 w-full h-[360px] absolute">
-        <Image
-          src="/blombruket-logotype-symbol-beige.svg"
-          width={192}
-          height={300}
-          alt="Blombrukets logotyp i symbolform"
-        />
-        <div>
-          <h6 className="text-xl uppercase mb-6">Information</h6>
-          <a href="#" className="block">
-            Nyhetsbrev
-          </a>
-          <a href="#" className="block">
-            Mina sidor
-          </a>
-          <a href="#" className="block">
-            Frågor och svar
-          </a>
-          <a href="#" className="block">
-            Våra villkor
-          </a>
-          <a href="#" className="block">
-            Integritetspolicy
-          </a>
-        </div>
-        <div>
-          <h6 className="text-xl uppercase">Kontakt</h6>
-          <a href="mailto:info@blombruket.se" className="block mt-6">
-            info@blombruket.se
-          </a>
-          <a href="tel:010-122 12 30" className="block">
-            010-122 12 30
-          </a>
-          <p>Södra vägen 30</p>
-          <p>392 45 Kalmar</p>
-          <p>559217-9567</p>
-          <p>VAT: SE559217956701</p>
-        </div>
-        <div className="flex flex-col">
-          <div className="flex items-center justify-end">
-            <h6 className="text-xl uppercase">Instagram</h6>
-            <Image
-              src="/instagram-logo-white.svg"
-              width={32}
-              height={32}
-              alt="Instagrams logotyp"
-              className="ml-3"
-            />
+    <footer className="bg-button-green text-white h-[516px] flex absolute w-full justify-center">
+      <div className="w-full md:w-3/4 h-full flex items-center absolute">
+        <div className="flex justify-evenly md:justify-between w-full h-[360px]">
+          <Image
+            src="/blombruket-logotype-symbol-beige.svg"
+            width={192}
+            height={300}
+            alt="Blombrukets logotyp i symbolform"
+            className="hidden md:block"
+          />
+          <div>
+            <h6 className="text-xl uppercase mb-6">Information</h6>
+            <a href="#" className="block">
+              Nyhetsbrev
+            </a>
+            <a href="#" className="block">
+              Mina sidor
+            </a>
+            <a href="#" className="block">
+              Frågor och svar
+            </a>
+            <a href="#" className="block">
+              Våra villkor
+            </a>
+            <a href="#" className="block">
+              Integritetspolicy
+            </a>
           </div>
-          <div className="flex items-center justify-end mt-3">
-            <h6 className="text-xl uppercase">Facebook</h6>
-            <Image
-              src="/facebook-logo-white.svg"
-              width={32}
-              height={32}
-              alt="Facebooks logotyp"
-              className="ml-3"
-            />
+          <div>
+            <h6 className="text-xl uppercase">Kontakt</h6>
+            <a href="mailto:info@blombruket.se" className="block mt-6">
+              info@blombruket.se
+            </a>
+            <a href="tel:010-122 12 30" className="block">
+              010-122 12 30
+            </a>
+            <p>Södra vägen 30</p>
+            <p>392 45 Kalmar</p>
+            <p>559217-9567</p>
+            <p>VAT: SE559217956701</p>
           </div>
-          <div className="flex items-center justify-end mt-5">
-            <Image
-              src="/eu-logo.svg"
-              width={120}
-              height={42}
-              alt="Europeiska unionens logotyp"
-            />
+          {/* Sociala medier och eu */}
+          <div className="hidden md:block flex-col">
+            <Socials />
           </div>
         </div>
       </div>
-
-      <div className="relative w-full flex gap-6 justify-center top-32">
+      <div className="flex justify-center items-end md:hidden w-full z-10 mb-10 gap-10">
+        <Socials />
+      </div>
+      {/* Betallösningar */}
+      <div className="hidden md:flex relative w-full gap-6 justify-center top-48">
         <Image
           src="/swish-logotype.svg"
           width={198}
