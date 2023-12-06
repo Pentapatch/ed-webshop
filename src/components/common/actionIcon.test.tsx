af41bee0e840cfb7caa2702576fa33afecc65759
Emelie Baker <emeliefnbaker@gmail.com>
2023-12-06 15:26:43 +0100

    changed config

--- src/components/common/actionIcon.test.tsx
+++ src/components/common/actionIcon.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ActionIcon from "./actionIcon";
 
