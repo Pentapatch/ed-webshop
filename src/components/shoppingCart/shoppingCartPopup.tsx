c1b4537161e6f0b142ddba13b6a4b712e4f0623e
Dennis Hankvist <newside-sys@hotmail.com>
2023-12-06 14:47:09 +0100

    Klicka utanför shoppingcart för att stänga den

--- src/components/shoppingCart/shoppingCartPopup.tsx
+++ src/components/shoppingCart/shoppingCartPopup.tsx
@@ -1,7 +1,7 @@
 import { ShoppingCart } from "@/models/shoppingCart";
 import { ShoppingCartEntry } from "@/models/shoppingCartEntry";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ActionButton from "../common/actionButton";
 import ShoppingCartList from "./shoppingCartList";
 
@@ -21,42 +21,65 @@ const ShoppingCartPopup: React.FC<ShoppingCartPopupProps> = ({
   onDecreaseCartItemQuantity,
   onIncreaseCartItemQuantity,
   onRemoveCartItem,
-}) => (
-  <>
-    {isOpen && (
-      <div className="max-w-[512px] w-full flex items-center justify-center fixed z-20 top-12 md:top-20 px-2 md:px-0">
-        <div className="w-full p-6 relative bg-white shadow-2xl">
-          {/* Close popup button */}
-          <button
-            className="absolute right-3 top-3"
-            onClick={onCloseShoppingCart}
-          >
-            <Image
-              src="/login-close.png"
-              alt={`Grafik för att stänga varukorgen`}
-              width={50}
-              height={50}
+}) => {
+  const popupRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
+      onCloseShoppingCart();
+    }
+  };
+
+  useEffect(() => {
+    // Attach the event listener when the component mounts
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Detach the event listener when the component unmounts
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <>
+      {isOpen && (
+        <div
+          ref={popupRef}
+          className="max-w-[512px] w-full flex items-center justify-center fixed z-20 top-12 md:top-20 px-2 md:px-0"
+        >
+          <div className="w-full p-6 relative bg-white shadow-2xl">
+            {/* Close popup button */}
+            <button
+              className="absolute right-3 top-3"
+              onClick={onCloseShoppingCart}
+            >
+              <Image
+                src="/login-close.png"
+                alt={`Grafik för att stänga varukorgen`}
+                width={50}
+                height={50}
+              />
+            </button>
+            {/* Title */}
+            <h6 className="text-center uppercase text-xl mb-2">Min varukorg</h6>
+            {/* List of shopping cart rows */}
+            <ShoppingCartList
+              cart={cart}
+              onDecreaseCartItemQuantity={onDecreaseCartItemQuantity}
+              onIncreaseCartItemQuantity={onIncreaseCartItemQuantity}
+              onRemoveCartItem={onRemoveCartItem}
             />
-          </button>
-          {/* Title */}
-          <h6 className="text-center uppercase text-xl mb-2">Min varukorg</h6>
-          {/* List of shopping cart rows */}
-          <ShoppingCartList
-            cart={cart}
-            onDecreaseCartItemQuantity={onDecreaseCartItemQuantity}
-            onIncreaseCartItemQuantity={onIncreaseCartItemQuantity}
-            onRemoveCartItem={onRemoveCartItem}
-          />
-          {/* Checkout button */}
-          <ActionButton
-            text="Till kassan"
-            onClick={() => {}}
-            className="w-full mt-4"
-          />
+            {/* Checkout button */}
+            <ActionButton
+              text="Till kassan"
+              onClick={() => {}}
+              className="w-full mt-4"
+            />
+          </div>
         </div>
-      </div>
-    )}
-  </>
-);
+      )}
+    </>
+  );
+};
 
 export default ShoppingCartPopup;
